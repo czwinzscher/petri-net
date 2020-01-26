741701388ed26382846a94e4732bd69393958868
clemens <czwinzscher2@gmail.com>
2020-01-26 15:00:23 +0100

    add tests for isEnabled and fire

--- test/Main.hs
+++ test/Main.hs
@@ -43,4 +43,49 @@ unitTests =
         post (Transition 'p') net @?= Set.fromList [Place 'B']
         post (Transition 'q') net @?= Set.fromList [Place 'A', Place 'C']
         post (Transition 'r') net @?= Set.fromList [Place 'A']
+    , testCase "enabled transitions / fire transitions" $ do
+        let net =
+              PetriNet
+                { places =
+                    Set.fromList
+                      [Place 'A', Place 'B', Place 'C', Place 'D', Place 'E']
+                , transitions =
+                    Set.fromList
+                      [ Transition 'q'
+                      , Transition 'p'
+                      , Transition 'r'
+                      , Transition 's'
+                      ]
+                , flowRelations =
+                    Set.fromList
+                      [ PT (Place 'A') (Transition 'p')
+                      , PT (Place 'B') (Transition 'q')
+                      , PT (Place 'C') (Transition 'q')
+                      , PT (Place 'C') (Transition 's')
+                      , PT (Place 'D') (Transition 's')
+                      , PT (Place 'E') (Transition 'r')
+                      , TP (Transition 'p') (Place 'B')
+                      , TP (Transition 'p') (Place 'C')
+                      , TP (Transition 'q') (Place 'A')
+                      , TP (Transition 'r') (Place 'C')
+                      , TP (Transition 'r') (Place 'D')
+                      , TP (Transition 's') (Place 'E')
+                      ]
+                , marking = Map.fromList [(Place 'A', 1), (Place 'E', 1)]
+                , weights =
+                    Map.fromList
+                      [ (TP (Transition 'q') (Place 'A'), 2)
+                      , (TP (Transition 's') (Place 'E'), 2)
+                      ]
+                }
+        isEnabled (Transition 'p') net @?= True
+        isEnabled (Transition 'q') net @?= False
+        isEnabled (Transition 'r') net @?= True
+        isEnabled (Transition 's') net @?= False
+        fire (Transition 'q') net @?= net
+        let net2 = fire (Transition 'p') net
+            expectedMarking2 =
+              Map.fromList
+                [(Place 'A', 0), (Place 'B', 1), (Place 'C', 1), (Place 'E', 1)]
+        marking net2 @?= expectedMarking2
     ]
